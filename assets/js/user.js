4d4c4c3bebc225b23b0b28698e824ee4f25f1c81
eduarda <eduarda-tavares1@educar.rs.gov.br>
2024-11-12 01:16:27 -0300

    alterações nas telas de login e user

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,31 +1,41 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const estagiarioDados = document.getElementById("estagiario-dados");
-    const idEstagiario = 3; // Substitua pelo id do estagiário que você deseja buscar
 
     try {
-        const response = await fetch(`http://localhost:3000/api/estagiario/${idEstagiario}`);
+        const token = localStorage.getItem("token"); // Supondo que o token está salvo no localStorage após login
+        console.log(localStorage.getItem("token"));
+
+        if (!token) {
+            estagiarioDados.innerHTML = '<p>Erro: Usuário não autenticado.</p>';
+            return;
+        }
+
+        const response = await fetch(`http://localhost:3000/api/perfil`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
+
         const estagiario = await response.json();
 
         if (estagiario) {
-            const estagiarioCard = document.createElement('div');
-            estagiarioCard.className = 'col-md-0 mb-4';
-            estagiarioCard.innerHTML = `
+            estagiarioDados.innerHTML = `
                 <div class="cardUser">
                     <div class="card-body">
                         <h5 class="card-title"><strong>${estagiario.nome}</strong></h5>
-                        <p class="card-text">Cidade: ${estagiario.cidade}</p>
-                        <p class="card-text">Habilidades: ${estagiario.habilidades}</p>
-                        <p class="card-text">Formação Acadêmica: ${estagiario.formacaoAcademica}</p>
-                        <p class="card-text">Telefone: ${estagiario.telefone}</p>
-                        <p class="card-text">Email: ${estagiario.email}</p>
-                        <p class="card-text">Links: ${estagiario.links}</p>
+                        <p class="card-text"><strong>Cidade:</strong> ${estagiario.cidade}</p>
+                        <p class="card-text"><strong>Habilidades:</strong> ${estagiario.habilidades}</p>
+                        <p class="card-text"><strong>Formação Acadêmica:</strong> ${estagiario.formacaoAcademica}</p>
+                        <p class="card-text"><strong>Telefone:</strong> ${estagiario.telefone}</p>
+                        <p class="card-text"><strong>Email:</strong> ${estagiario.email}</p>
+                        <p class="card-text"><strong>Links:</strong> ${estagiario.links}</p>
                     </div>
                 </div>
             `;
-            estagiarioDados.appendChild(estagiarioCard); // Corrigido aqui para usar estagiarioDados
         } else {
             estagiarioDados.innerHTML = '<p>Estagiário não encontrado.</p>';
         }
